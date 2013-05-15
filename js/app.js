974d9f05f6ce4e039b78dbd291910453b676b666
Sri <srisreed@gmail.com>
2013-05-14 20:59:56 -0700

    updated css & html, added favicon, js

---
+++ js/app.js
@@ -0,0 +1,27 @@
+$(function(){  
+    $('#basecamp').hover(function(){
+        $('.title > h1').toggle();
+        $('.title > h3').toggle();
+        $('.bc').toggle();
+    });
+
+    $('#highrise').hover(function(){
+        $('.title > h1').toggle();
+        $('.title > h3').toggle();
+        $('.hr').toggle();
+    });
+
+    $('#campfire').hover(function(){
+        $('.title > h1').toggle();
+        $('.title > h3').toggle();
+        $('.cf').toggle();
+    });
+
+    var d, today, days;
+   
+    d = new Date();
+    today = d.getDay();
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+   
+    $('#day').append("<a href=\"#\">Happy " + days[today] + ".</a>");
+});
